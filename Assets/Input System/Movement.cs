ad191e52921a18b530d7b6fe31a9e61bb7084617
Knasse01 <kevin.tang.swe@gmail.com>
2023-07-25 06:54:38 +0200

    Fixed movement

--- Assets/Input System/Movement.cs
+++ Assets/Input System/Movement.cs
@@ -6,10 +6,14 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     private Rigidbody playerRigidBody;
+    Vector2 inputVector;
+    [SerializeField]
+    private float movementSpeed;
 
     private void Awake()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+        
     }
     public void Jump(InputAction.CallbackContext context) 
     {
@@ -19,8 +23,18 @@ public class Movement : MonoBehaviour
             playerRigidBody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
         }
     }
+    public void Update()
+    {
+        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y) * movementSpeed * Time.fixedDeltaTime;
+
+        // Move the player using the calculated movement vector
+        playerRigidBody.MovePosition(playerRigidBody.position + movement);
+        //playerRigidBody.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * movementSpeed, ForceMode.Force);
+    }
+
     public void Moving(InputAction.CallbackContext context) 
     {
+        inputVector = context.ReadValue<Vector2>();
         Debug.Log(context);
     }
 }
